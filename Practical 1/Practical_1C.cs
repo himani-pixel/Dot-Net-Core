68d3c27b4bbd94e70df80f0d52443103d77d3383
Himani Malankar <himanimalankar@gmail.com>
2026-07-14 23:24:26 +0530

    Add files via upload

---
+++ Practical 1/Practical_1C.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+
+//class Practical_1C
+//{
+//    public static void Main(string[] args)
+//    {
+//        int count = 1;
+
+//        for (int i = 1; i <= 5; i++)
+//        {
+//            for (int j = 1; j <= i; j++)
+//            {
+//                Console.Write(count + " ");
+//                count++;
+//            }
+
+//            Console.WriteLine();
+//        }
+
+//        Console.WriteLine("Himani Malankar T016");
+//        Console.ReadLine();
+//    }
+//}
