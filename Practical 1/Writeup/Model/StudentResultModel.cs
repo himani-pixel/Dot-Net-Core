81331a8a1d4130264dc62164d21ae851cc62b9c1
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 20:44:13 +0530

    Add files via upload

---
+++ Practical 1/Writeup/Model/StudentResultModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Practical_1_Writeup.Models
+{
+    public class StudentResultModel
+    {
+        public string RollNo { get; set; }
+        public string Name { get; set; }
+
+        public int Sub1 { get; set; }
+        public int Sub2 { get; set; }
+        public int Sub3 { get; set; }
+        public int Sub4 { get; set; }
+        public int Sub5 { get; set; }
+
+        public int Total { get; set; }
+        public double Percentage { get; set; }
+        public string Grade { get; set; }
+    }
+}
